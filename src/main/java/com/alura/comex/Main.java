d8e28a20a3ca5fd7bafb2af0e1b09a4b68fcade0
=RonnyB <=rjba08@gmail.com>
2025-01-15 09:37:49 -0600

    extraer procesamiento de csv de clase main

--- src/main/java/com/alura/comex/Main.java
+++ src/main/java/com/alura/comex/Main.java
@@ -6,7 +6,7 @@ import java.util.*;
 
 public class Main {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         ProcesadorDeCsv procesadorDeCsv = new ProcesadorDeCsv();
         ArrayList<Pedido> pedidos = procesadorDeCsv.listaDePedidos("pedidos.csv");
 
