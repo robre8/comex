a288ad5fd21e0330655329ca799612e336eeb03b
Ronny <rjba08@gmail.com>
2025-02-11 15:49:42 -0600

    Nueva rama modificado el ProcesadorDeCsv para docker con un jar

--- src/main/java/com/alura/comex/ProcesadorDeCsv.java
+++ src/main/java/com/alura/comex/ProcesadorDeCsv.java
@@ -4,10 +4,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.math.BigDecimal;
-import java.net.URISyntaxException;
-import java.net.URL;
 import java.nio.charset.StandardCharsets;
-import java.nio.file.Path;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
@@ -18,29 +15,14 @@ public class ProcesadorDeCsv implements Procesador {
     @Override
     public ArrayList<Pedido> procesar(String rutaArchivo) throws IOException {
         ArrayList<Pedido> pedidos = new ArrayList<>();
-//        try {
-//            URL recurso = ClassLoader.getSystemResource(rutaArchivo);
-//            Path ruta = Path.of(recurso.toURI());
-//
-//            try (Scanner scanner = new Scanner(ruta)) {
-//                scanner.nextLine(); // Saltar cabecera
-//                while (scanner.hasNextLine()) {
-//                    String[] campos = scanner.nextLine().split(",");
-//                    pedidos.add(mapearAPedido(campos));
-//                }
-//            }
-//        } catch (URISyntaxException e) {
-//            throw new IOException("Error en la ruta del archivo: " + rutaArchivo, e);
-//        }
-//        return pedidos;
-//    }
+
         try (InputStream inputStream = getClass().getClassLoader().getResourceAsStream(rutaArchivo)) {
             if (inputStream == null) {
                 throw new IOException("No se pudo encontrar el archivo: " + rutaArchivo);
             }
 
             try (Scanner scanner = new Scanner(new InputStreamReader(inputStream, StandardCharsets.UTF_8))) {
-                scanner.nextLine(); // Saltar cabecera
+                scanner.nextLine();
                 while (scanner.hasNextLine()) {
                     String[] campos = scanner.nextLine().split(",");
                     pedidos.add(mapearAPedido(campos));
